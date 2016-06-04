796a63e092eff1f85add2129fd090fff35e4543e
Eduardo K <eduard@kozachek.net>
2016-06-04 12:39:57 +0300

    Added html support

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ export default {
 
         let msg = document.createElement('div');
         msg.className = 'vue-loading-msg';
-        msg.textContent = this.options.text;
+        msg.innerHTML = this.options.text;
         box.appendChild(msg);
 
         window.requestAnimationFrame(() => {
